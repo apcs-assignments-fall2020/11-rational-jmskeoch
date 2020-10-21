f79112f099aeca9aa2b7b446847a49442d96abae
jmskeoch <jmskeoch@yahoo.com>
2020-10-21 16:25:21 +0000

    Add basic math methods and simplify

--- src/main/java/Rational.java
+++ src/main/java/Rational.java
@@ -50,28 +50,35 @@ public class Rational
     //      simplify(1/2) => 1/2
     public static Rational simplify(Rational r)
     {
-        // REPLACE WITH YOUR CODE HERE
-        return null;
+        int gcd = greatestCommonFactor(r.numerator, r.denominator);
+        Rational newRational = new Rational(r.numerator / gcd, r.denominator / gcd);
+        return newRational;
     }
 
     // This method takes two Rationals, subtracts thems up, 
     // and returns a Rational equal to the difference
     public static Rational subtract(Rational r, Rational s)
     {
-        // REPLACE WITH YOUR CODE HERE
-        return null;
+        int newNumerator = r.numerator*s.denominator - s.numerator*r.denominator;
+        int newDenominator = r.denominator*s.denominator; 
+        Rational x = new Rational(newNumerator, newDenominator);
+        return simplify(x);
     }
     
     public static Rational multiply(Rational r, Rational s)
     {
-        // REPLACE WITH YOUR CODE HERE
-        return null;
+        int newNumerator = r.numerator*s.numerator;
+        int newDenominator = r.denominator*s.denominator; 
+        Rational x = new Rational(newNumerator, newDenominator);
+        return simplify(x);
     }
     
     public static Rational divide(Rational r, Rational s)
     {
-        // REPLACE WITH YOUR CODE HERE
-        return null;
+        int newNumerator = r.numerator*s.denominator;
+        int newDenominator = r.denominator*s.numerator; 
+        Rational x = new Rational(newNumerator, newDenominator);
+        return simplify(x);
     }
 
 
